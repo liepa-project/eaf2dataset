f991c2c91a42f556c098105fc6b105c5bed76fc9
Mondhs Bus <mondhs@gmail.com>
2025-07-31 23:13:26 +0300

    not deleting sentence symbols

--- bin/reformat_tsv.sh
+++ bin/reformat_tsv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 tsv_file="$1"
 while IFS=$'\t' read -r input_wav output_mp3 start_segment end_segment duration annotation ; do
-    clean_annotation=$(echo "$annotation" | sed "s/[|,\"\.?!]//g")
+    clean_annotation=$(echo "$annotation" | sed "s/[|,\"]//g")
     echo "./mp3/$output_mp3,$clean_annotation"
 done < "$tsv_file"
