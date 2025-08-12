10e1d3d7fb164c9130c0dcd41f440ec959d1f469
Mondhs Bus <mondhs@gmail.com>
2025-08-12 08:25:23 +0300

    Fixed reformating

--- src/parse_eaf.py
+++ src/parse_eaf.py
@@ -243,8 +243,9 @@ def format_annotations(eaf_doc:AnnotationDoc, group_annotations:List[Annotation]
         #bash script loosing first symbols. I ll us currend dir hack
         aStr = f"./././././{media_dir_name}/{media_file_name_wo_ext}.wav\t{media_dir_name}/{media_file_name_wo_ext}_chunk_{counter:03}.mp3\t{annotation.time_slot_start}\t{annotation.time_slot_end}\t{segment_length}\t{annotation_value_len}\t{annotation_value}"
         ### workarounds
-        if(annotation_value_len>1000):
-           aStr = f"###{aStr}"
+        if(annotation_value_len>700):
+        #    aStr = f"###{aStr}"
+           continue
 
 
         result.append(aStr)
